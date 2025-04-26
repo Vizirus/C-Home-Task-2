c59185d1d7ab39aa8fb207afed33430cbe819987
artem <denloken@gmail.com>
2025-04-26 23:45:33 +0300

    Added the JSON serialization method and implemented logic of parsing

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace HomeWrokOOP_2
+﻿using HomeWrokOOP_2.Users;
+using System.Text.Json;
+
+namespace HomeWrokOOP_2
 {
     internal class Program
     {
+        private static void serializeJson(string nameAndPath, User user)
+        {
+            string json = JsonSerializer.Serialize(user);
+            using (StreamWriter sw = new StreamWriter(nameAndPath))
+            {
+                sw.Write(json);
+                sw.Close();
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to JSON Serialize/deserialize");
+            User user1 = new User() { Name = "Jhon", Email = "jhon@gmail.com", Id = 13313123, Surname = "Hornet" };
+            Admin admin = new Admin() { Name = "Admin", Email = "email@gmail.com", Age = 52, Id = 1425252, Role = "System admin", Surname = "Admin" };
+            serializeJson("user1.json", user1);
+
+            var streamJson = File.ReadAllText("user1.json");
+            User jsonDes = JsonSerializer.Deserialize<User>(streamJson);
+            Console.WriteLine("Parsed user = {0}, {1}, {2}, {3}", jsonDes.Id, jsonDes.Surname, jsonDes.Email, jsonDes.Name);
         }
     }
 }
