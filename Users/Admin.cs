1524b97e3376f1acce5e0a5e1605a9d7af5679b4
artem <denloken@gmail.com>
2025-04-26 23:23:07 +0300

    First commit (Base Added)

---
+++ Users/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWrokOOP_2.Users
+{
+    internal class Admin: User
+    {
+        public int Age { get; set; }
+        public string Role { get; set; }
+    }
+}
